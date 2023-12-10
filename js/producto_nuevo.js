017d8c220b18f11fb2160b6ea5f05b465484eb10
Catriel <Lucascatrielb@gmail.com>
2023-12-10 16:42:39 -0300

    1

--- js/producto_nuevo.js
+++ js/producto_nuevo.js
@@ -21,7 +21,7 @@ function guardar() {
         .then(function () {
             console.log("creado")
             alert("Grabado")
-            window.location.href = "/controlPanel.html";  
+            window.location.href = "/pages/controlpanel.html";  
             // Handle response we get from the API
         })
         .catch(err => {
